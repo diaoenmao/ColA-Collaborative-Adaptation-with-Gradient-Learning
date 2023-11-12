fix dist, polish test computation and clean verify

src/train_cola.py
@@ -147,7 +147,7 @@ def train(data_loader, model, cola_base, optimizer, scheduler, metric, logger):
         model.zero_grad()
         input_i, output_target_i = model.flush()
         if cfg['test_computation']:
-            cfg['time_backward'].append(time.time() - s)
+            cfg['time_used'].append(time.time() - s)
         for k in input_i:
             input_buffer[k].append(input_i[k])
             output_target_buffer[k].append(output_target_i[k])
@@ -162,7 +162,7 @@ def train(data_loader, model, cola_base, optimizer, scheduler, metric, logger):
                 input_cola = {'data': input_cola, 'target': output_target_cola}
                 cola_base[k].fit(input_cola, optimizer[k], scheduler[k])
                 if cfg['test_computation']:
-                    cfg['time_cola'].append(time.time() - s)
+                    cfg['time_used_cola'].append(time.time() - s)
             input_buffer = defaultdict(list)
             output_target_buffer = defaultdict(list)
         evaluation = metric.evaluate('train', 'batch', input_, output_)
@@ -186,16 +186,13 @@ def train(data_loader, model, cola_base, optimizer, scheduler, metric, logger):
                 mem_free_cola, mem_total_cola = torch.cuda.mem_get_info(cfg['device_cola'])
                 cfg['mem_used_cola'].append(mem_total_cola - mem_free_cola)
             if i == cfg['num_test_iter']:
-                print(cfg['time_backward'])
-                print(cfg['time_cola'])
-                print(cfg['mem_used'])
-                print(cfg['mem_used_cola'])
-                print('Run time backward: {}/{}'.format(np.mean(cfg['time_backward'][1:]),
-                                                        np.std(cfg['time_backward'][1:])))
-                print('Run time (ColA): {}/{}'.format(np.mean(cfg['time_cola'][1:]),
-                                                      np.std(cfg['time_cola'][1:])))
-                print('Memory used: {}/{}'.format(np.mean(cfg['mem_used'][1:]),
-                                                  np.std(cfg['mem_used'][1:])))
+                print('Run time backward: {}({})'.format(np.mean(cfg['time_used'][1:]),
+                                                         np.std(cfg['time_used'][1:])))
+                print('Run time (ColA, M={}): {}({})'.format(len(list(cola_base.keys())),
+                                                             np.mean(cfg['time_used_cola'][1:]),
+                                                             np.std(cfg['time_used_cola'][1:])))
+                print('Memory used: {}/({})'.format(np.mean(cfg['mem_used'][1:]),
+                                                    np.std(cfg['mem_used'][1:])))
                 if cfg['device_cola'] != 'cpu':
                     print('Memory used (ColA): {}/{}'.format(np.mean(cfg['mem_used_cola'][1:]),
                                                              np.std(cfg['mem_used_cola'][1:])))
@@ -206,9 +203,9 @@ def train(data_loader, model, cola_base, optimizer, scheduler, metric, logger):
 def test(data_loader, model, cola_base, metric, logger):
     with torch.no_grad():
         model.train(False)
+        for k in cola_base:
+            cola_base[k] = cola_base[k].to(cfg['device'])
         for i, input in enumerate(data_loader):
-            for k in cola_base:
-                cola_base[k] = cola_base[k].to(cfg['device'])
             if cfg['task_name'] in ['s2s', 'sc', 'clm']:
                 input_size = input['labels'].size(0)
                 input = {'input_ids': input['input_ids'], 'attention_mask': input['attention_mask'],

src/train_cola_dist.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+import numpy as np
 import os
 import shutil
 import time
@@ -9,7 +10,7 @@ from collections import defaultdict
 from config import cfg, process_args
 from dataset import make_dataset, make_data_loader, process_dataset, collate
 from metric import make_metric, make_logger
-from model import make_model, make_optimizer, make_scheduler, make_ft_model, freeze_model, make_cola
+from model import make_model, make_optimizer, make_scheduler, make_ft_model, freeze_model, unfreeze_model, make_cola
 from module import save, to_device, process_control, resume, makedir_exist_ok, PeftModel
 
 cudnn.benchmark = True
@@ -40,7 +41,7 @@ def runExperiment():
     model_tag_path = os.path.join(model_path, cfg['model_tag'])
     checkpoint_path = os.path.join(model_tag_path, 'checkpoint')
     best_path = os.path.join(model_tag_path, 'best')
-    dataset = make_dataset(cfg['data_name'])
+    dataset = make_dataset(cfg['data_name'], cfg['subset_name'])
     model, tokenizer = make_model(cfg['model_name'])
     dataset = process_dataset(dataset, tokenizer)
     data_loader = make_data_loader(dataset, tokenizer, cfg['model_name'])
@@ -57,12 +58,16 @@ def runExperiment():
         model.load_cola_base(cola_base)
         optimizer = defaultdict(list)
         scheduler = defaultdict(list)
+        num_params = 0
         for k in cola_base:
             for i in range(cfg['num_split']):
-                cola_base[k].model[i] = cola_base[k].model[i].to(cfg['device'])
+                for n, p in cola_base[k].model[i].named_parameters():
+                    num_params += p.numel()
+                cola_base[k].model[i] = cola_base[k].model[i].to(cfg['device_cola'])
                 cola_param_k_i = cola_base[k].model[i].parameters()
                 optimizer[k].append(make_optimizer(cola_param_k_i, 'cola'))
                 scheduler[k].append(make_scheduler(optimizer[k][i], 'cola'))
+        print("Number of ColA trainable parameters: {}".format(num_params))
     else:
         cfg['epoch'] = result['epoch']
         model = PeftModel.from_pretrained(model, os.path.join(checkpoint_path, 'adapter'), is_trainable=True)
@@ -76,14 +81,18 @@ def runExperiment():
         model.load_cola_base(cola_base)
         optimizer = defaultdict(list)
         scheduler = defaultdict(list)
+        num_params = 0
         for k in cola_base:
             for i in range(cfg['num_split']):
-                cola_base[k].model[i] = cola_base[k].model[i].to(cfg['device'])
+                for n, p in cola_base[k].model[i].named_parameters():
+                    num_params += p.numel()
+                cola_base[k].model[i] = cola_base[k].model[i].to(cfg['device_cola'])
                 cola_param_k_i = cola_base[k].model[i].parameters()
                 optimizer[k].append(make_optimizer(cola_param_k_i, 'cola'))
                 scheduler[k].append(make_scheduler(optimizer[k][i], 'cola'))
                 optimizer[k][i].load_state_dict(result['optimizer_state_dict'][k][i])
                 scheduler[k][i].load_state_dict(result['scheduler_state_dict'][k][i])
+        print("Number of ColA trainable parameters: {}".format(num_params))
         metric.load_state_dict(result['metric_state_dict'])
         logger.load_state_dict(result['logger_state_dict'])
     for epoch in range(cfg['epoch'], cfg[cfg['model_name']]['num_epochs'] + 1):
@@ -110,24 +119,6 @@ def runExperiment():
     return
 
 
-def sum_loss(logits, labels):
-    num_labels = logits.size(-1)
-    if labels is not None:
-        if num_labels == 1:
-            #  We are doing regression
-            loss_fct = torch.nn.MSELoss(reduction='sum')
-            loss = loss_fct(logits.view(-1), labels.view(-1))
-        else:
-            if cfg['task_name'] == 'clm':
-                logits = logits[..., :-1, :].contiguous()
-                labels = labels[..., 1:].contiguous()
-            loss_fct = torch.nn.CrossEntropyLoss(reduction='sum')
-            loss = loss_fct(logits.view(-1, num_labels), labels.view(-1))
-    else:
-        loss = 0
-    return loss
-
-
 def train(data_loader, model, cola_base, optimizer, scheduler, metric, logger):
     model.train(True)
     start_time = time.time()
@@ -138,8 +129,13 @@ def train(data_loader, model, cola_base, optimizer, scheduler, metric, logger):
         split_i = input['split']
         for k in cola_base:
             lr = optimizer[k][0].param_groups[0]['lr']
-            cola_base[k].train(False)
+            for j in range(len(cola_base[k].model)):
+                cola_base[k].model[j] = cola_base[k].model[j].to(cfg['device'])
+                cola_base[k].model[j].train(False)
+                freeze_model(cola_base[k].model[j])
             cola_base[k].make_split(split_i)
+        if cfg['test_computation']:
+            s = time.time()
         input_size = input['labels'].size(0)
         input = {'input_ids': input['input_ids'], 'attention_mask': input['attention_mask'],
                  'labels': input['labels']}
@@ -147,22 +143,31 @@ def train(data_loader, model, cola_base, optimizer, scheduler, metric, logger):
         output = model(**input)
         input_ = {'target': input['labels']}
         output_ = {'target': output['logits'], 'loss': output['loss']}
-        loss = sum_loss(output['logits'], input['labels'])
+        loss = output['loss']
         loss.backward()
         model.zero_grad()
         input_i, output_target_i = model.flush()
+        if cfg['test_computation']:
+            cfg['time_used'].append(time.time() - s)
         for k in input_i:
             input_buffer[k].append(input_i[k])
             output_target_buffer[k].append(output_target_i[k])
             split_buffer[k].append(split_i)
         if (i + 1) % cfg['cola']['num_steps'] == 0:
             for k in input_buffer:
+                if cfg['test_computation']:
+                    s = time.time()
+                for j in range(len(cola_base[k].model)):
+                    cola_base[k].model[j] = cola_base[k].model[j].to(cfg['device_cola'])
+                    unfreeze_model(cola_base[k].model[j])
                 input_cola = torch.cat(input_buffer[k], dim=0)
                 output_target_cola = torch.cat(output_target_buffer[k], dim=0)
                 split_cola = torch.cat(split_buffer[k], dim=0)
                 cola_base[k].make_split(split_cola)
                 input_cola = {'data': input_cola, 'target': output_target_cola}
                 cola_base[k].fit(input_cola, optimizer[k], scheduler[k])
+                if cfg['test_computation']:
+                    cfg['time_used_cola'].append(time.time() - s)
             input_buffer = defaultdict(list)
             output_target_buffer = defaultdict(list)
             split_buffer = defaultdict(list)
@@ -181,12 +186,34 @@ def train(data_loader, model, cola_base, optimizer, scheduler, metric, logger):
                              'Experiment Finished Time: {}'.format(exp_finished_time)]}
             logger.append(info, 'train')
             print(logger.write('train', metric.metric_name['train']))
+        if cfg['test_computation']:
+            mem_free, mem_total = torch.cuda.mem_get_info(cfg['device'])
+            cfg['mem_used'].append(mem_total - mem_free)
+            if cfg['device_cola'] != 'cpu':
+                mem_free_cola, mem_total_cola = torch.cuda.mem_get_info(cfg['device_cola'])
+                cfg['mem_used_cola'].append(mem_total_cola - mem_free_cola)
+            if i == cfg['num_test_iter']:
+                print('Run time backward: {}({})'.format(np.mean(cfg['time_used'][1:]),
+                                                         np.std(cfg['time_used'][1:])))
+                print('Run time (ColA, M={}, K={}): {}({})'.format(len(list(cola_base.keys())),
+                                                                   len(cola_base[k].model),
+                                                                   np.mean(cfg['time_used_cola'][1:]),
+                                                                   np.std(cfg['time_used_cola'][1:])))
+                print('Memory used: {}/({})'.format(np.mean(cfg['mem_used'][1:]),
+                                                    np.std(cfg['mem_used'][1:])))
+                if cfg['device_cola'] != 'cpu':
+                    print('Memory used (ColA): {}/{}'.format(np.mean(cfg['mem_used_cola'][1:]),
+                                                             np.std(cfg['mem_used_cola'][1:])))
+                exit()
     return
 
 
 def test(data_loader, model, cola_base, metric, logger):
     with torch.no_grad():
         model.train(False)
+        for k in cola_base:
+            for i in range(len(cola_base[k].model)):
+                cola_base[k].model[i] = cola_base[k].model[i].to(cfg['device'])
         for i, input in enumerate(data_loader):
             for k in cola_base:
                 cola_base[k].make_split(input['split'])

src/train_peft.py
@@ -114,7 +114,7 @@ def train(data_loader, model, optimizer, scheduler, metric, logger):
         scheduler.step()
         optimizer.zero_grad()
         if cfg['test_computation']:
-            cfg['time_backward'].append(time.time() - s)
+            cfg['time_used'].append(time.time() - s)
         evaluation = metric.evaluate('train', 'batch', input_, output_)
         logger.append(evaluation, 'train', n=input_size)
         if i % int((len(data_loader) * cfg['log_interval']) + 1) == 0:
@@ -133,11 +133,9 @@ def train(data_loader, model, optimizer, scheduler, metric, logger):
             mem_free, mem_total = torch.cuda.mem_get_info(cfg['device'])
             cfg['mem_used'].append(mem_total - mem_free)
             if i == cfg['num_test_iter']:
-                print(cfg['time_backward'])
-                print(cfg['mem_used'])
-                print('Run time backward: {}/{}'.format(np.mean(cfg['time_backward'][1:]),
-                                                        np.std(cfg['time_backward'][1:])))
-                print('Memory used: {}/{}'.format(np.mean(cfg['mem_used'][1:]),
+                print('Run time backward: {}({})'.format(np.mean(cfg['time_used'][1:]),
+                                                        np.std(cfg['time_used'][1:])))
+                print('Memory used: {}({})'.format(np.mean(cfg['mem_used'][1:]),
                                                   np.std(cfg['mem_used'][1:])))
                 exit()
     return

src/verify.py
@@ -22,70 +22,88 @@ def verify_gl():
         grad_pool.append(grad_)
         return
 
-    N = 1
-    D_1 = 5
-    D_2 = 5
-    S = 1
+    # Initialize data, target and original parameters
+    N = 10
+    D_1 = 200
+    D_2 = 100
     C = 2
-
-    # input_1 = torch.arange(N * D_1).float().view(N, D_1)
-    # weight_1 = torch.arange(D_1 * D_2).float().view(D_1, D_2)
-    # weight_2 = torch.arange(D_2 * C).float().view(D_2, C)
     input_1 = torch.randn((N, D_1))
     weight_1 = torch.randn((D_1, D_2))
     weight_2 = torch.randn((D_2, C))
+    target = torch.randint(0, C, (N,), dtype=torch.long)
+
+    print('--------Classical Backprop---------')
+    # Original parameters require gradient
     weight_1.requires_grad = True
     weight_2.requires_grad = True
-
+    # First layer
     output_1 = F.linear(input_1, weight_1.t())
-    output_1.register_hook(backward_hook)
     input_2 = F.relu(output_1)
-
-    # input_2 = output_1
+    # Second layer
     output_2 = F.linear(input_2, weight_2.t())
-    output_2.register_hook(backward_hook)
-    target = torch.zeros((N,), dtype=torch.long)
-
+    # Compute loss
     loss = F.cross_entropy(output_2, target, reduction='mean')
-    print(input_1)
-    print(weight_1)
-    print(output_1)
-    print(input_2)
-    print(weight_2)
-    print(output_2)
-    print(loss)
-    print('-----------------')
     loss.backward()
+    print('Loss: {}'.format(loss))
+    # Save gradient for comparison
     weight_1_grad = weight_1.grad
     weight_2_grad = weight_2.grad
+
+    print('--------Gradient Learning---------')
+    # Proposed method
+    # Backward original loss
+    # Use the same data and parameters
+    input_1_gl = input_1.clone().detach()
+    weight_1_gl = weight_1.clone().detach()
+    weight_2_gl = weight_2.clone().detach()
+    target_gl = target.clone().detach()
+    # Freeze original parameters
+    weight_1_gl.requires_grad = False
+    weight_2_gl.requires_grad = False
+    # First layer
+    output_1_gl = F.linear(input_1_gl, weight_1_gl.t())
+    output_1_gl.requires_grad_(True)
+    # register hook to retrieve gradient of hidden representations
+    output_1_gl.register_hook(backward_hook)
+    input_2_gl = F.relu(output_1_gl)
+    # Second layer
+    output_2_gl = F.linear(input_2_gl, weight_2_gl.t())
+    # register hook to retrieve gradient of hidden representations
+    output_2_gl.requires_grad_(True)
+    output_2_gl.register_hook(backward_hook)
+    # Compute loss
+    loss = F.cross_entropy(output_2_gl, target_gl, reduction='mean')
+    loss.backward()
+    print('Loss: {}'.format(loss))
+
+    # Backward on original parameters
+    # Use the same data and parameters
+    input_1_gl = input_1.clone().detach()
+    weight_1_gl = weight_1.clone().detach()
+    weight_2_gl = weight_2.clone().detach()
+    # Retrieve hidden representations and gradient of hidden representations
+    output_1_gl = output_1_gl.clone().detach()
+    input_2_gl = input_2_gl.clone().detach()
+    output_2_gl = output_2_gl.clone().detach()
     output_1_grad = grad_pool[1]
     output_2_grad = grad_pool[0]
-    print(weight_1_grad)
-    print(weight_2_grad)
-    print(output_1_grad)
-    print(output_2_grad)
-    print('-----------------')
-    input_1_gl = input_1.clone().detach()
-    output_1_gl = output_1.clone().detach()
-    weight_1_gl = weight_1.clone().detach()
+    # First layer
     weight_1_gl.requires_grad = True
     output_1_gl_ = F.linear(input_1_gl, weight_1_gl.t())
-    gl_loss_1 = 0.5 * F.mse_loss(output_1_gl_, output_1_gl - output_1_grad.detach(), reduction='sum')
+    gl_loss_1 = 0.5 * F.mse_loss(output_1_gl_, (output_1_gl - output_1_grad).detach(), reduction='sum')  # Proposition 1
     gl_loss_1.backward()
-    weight_1_gl_grad = weight_1_gl.grad
-    print(weight_1_gl_grad)
-    print(torch.allclose(weight_1_gl_grad, weight_1_grad, atol=1e-05))
-    print('-----------------')
-    input_2_gl = input_2.clone().detach()
-    output_2_gl = output_2.clone().detach()
-    weight_2_gl = weight_2.clone().detach()
+    # Second layer
     weight_2_gl.requires_grad = True
     output_2_gl_ = F.linear(input_2_gl, weight_2_gl.t())
-    gl_loss_2 = 0.5 * F.mse_loss(output_2_gl_, output_2_gl - output_2_grad.detach(), reduction='sum')
+    gl_loss_2 = 0.5 * F.mse_loss(output_2_gl_, (output_2_gl - output_2_grad).detach(), reduction='sum')  # Proposition 1
     gl_loss_2.backward()
+    # Verification
+    weight_1_gl_grad = weight_1_gl.grad
     weight_2_gl_grad = weight_2_gl.grad
-    print(weight_2_gl_grad)
-    print(torch.allclose(weight_2_gl_grad, weight_2_grad, atol=1e-05))
+    if_match_1 = torch.allclose(weight_1_gl_grad, weight_1_grad, atol=1e-03)
+    if_match_2 = torch.allclose(weight_2_gl_grad, weight_2_grad, atol=1e-03)
+    print('[Verify Proposition 1] Gradient of original parameters (first layer): {}, '
+          'Gradient of original parameters (second layer): {}'.format(if_match_1, if_match_2))
     return
 
 
@@ -97,148 +115,191 @@ def verify_cola():
         grad_pool.append(grad_)
         return
 
-    N = 1
-    D_1 = 5
-    D_2 = 5
-    S = 1
+    # Initialize data, target, original parameters and auxiliary parameters
+    N = 10
+    D_1 = 200
+    D_2 = 100
     C = 2
-
-    # input_1 = torch.arange(N * D_1).float().view(N, D_1)
-    # weight_1 = torch.arange(D_1 * D_2).float().view(D_1, D_2)
-    # weight_2 = torch.arange(D_2 * C).float().view(D_2, C)
-    # weight_1_aux = torch.arange(D_1 * D_2).float().view(D_1, D_2)
-    # weight_2_aux = torch.arange(D_2 * C).float().view(D_2, C)
+    # Initialize data and weight
     input_1 = torch.randn((N, D_1))
     weight_1 = torch.randn((D_1, D_2))
     weight_2 = torch.randn((D_2, C))
     weight_1_aux = torch.randn((D_1, D_2))
     weight_2_aux = torch.randn((D_2, C))
+
+    print('--------Classical Backprop (LoRA-like)---------')
+    # Freeze original parameters and auxiliary parameters require gradient
     weight_1.requires_grad = False
     weight_2.requires_grad = False
     weight_1_aux.requires_grad = True
     weight_2_aux.requires_grad = True
-
+    # First layer
     output_1 = F.linear(input_1, weight_1.t())
     output_1_aux = F.linear(input_1, weight_1_aux.t())
     output_1_aux.register_hook(backward_hook)
     output_1 = output_1 + output_1_aux
     output_1.register_hook(backward_hook)
-
     input_2 = F.relu(output_1)
-    # input_2 = output_1
+    # Second Layer
     output_2 = F.linear(input_2, weight_2.t())
     output_2_aux = F.linear(input_2, weight_2_aux.t())
     output_2_aux.register_hook(backward_hook)
     output_2 = output_2 + output_2_aux
     output_2.register_hook(backward_hook)
-
     target = torch.zeros((N,), dtype=torch.long)
+    # Compute loss
     loss = F.cross_entropy(output_2, target, reduction='mean')
-    print(input_1)
-    print(weight_1)
-    print(weight_1_aux)
-    print(output_1)
-    print(input_2)
-    print(weight_2)
-    print(weight_2_aux)
-    print(output_2)
-    print(loss)
-    print('-----------------')
+    print('Loss: {}'.format(loss))
     loss.backward()
-    weight_1_grad = weight_1.grad
-    weight_2_grad = weight_2.grad
+    # Save gradient for comparison
     weight_1_aux_grad = weight_1_aux.grad
     weight_2_aux_grad = weight_2_aux.grad
+    # Verification
     output_1_aux_grad = grad_pool[3]
     output_1_grad = grad_pool[2]
     output_2_aux_grad = grad_pool[1]
     output_2_grad = grad_pool[0]
-    print(weight_1_grad)
-    print(weight_2_grad)
-    print(weight_1_aux_grad)
-    print(weight_2_aux_grad)
-    print(output_1_grad)
-    print(output_2_grad)
-    print(output_1_aux_grad)
-    print(output_2_aux_grad)
-    print(torch.allclose(output_1_grad, output_1_aux_grad, atol=1e-05))
-    print(torch.allclose(output_2_grad, output_2_aux_grad, atol=1e-05))
-    print('-----------------')
-    input_1_cola = input_1.clone().detach()
-    output_1_cola = output_1_aux.clone().detach()
-    weight_1_cola = weight_1_aux.clone().detach()
-    weight_1_cola.requires_grad = True
-    output_1_cola_ = F.linear(input_1_cola, weight_1_cola.t())
-    cola_loss_1 = 0.5 * F.mse_loss(output_1_cola_, output_1_cola - output_1_grad.detach(), reduction='sum')
-    cola_loss_1.backward()
-    weight_1_cola_grad = weight_1_cola.grad
-    print(weight_1_cola_grad)
-    print(torch.allclose(weight_1_cola_grad, weight_1_aux_grad, atol=1e-05))
-    print('-----------------')
-    input_2_cola = input_2.clone().detach()
-    output_2_cola = output_2_aux.clone().detach()
-    weight_2_cola = weight_2_aux.clone().detach()
-    weight_2_cola.requires_grad = True
-    output_2_cola_ = F.linear(input_2_cola, weight_2_cola.t())
-    cola_loss_2 = 0.5 * F.mse_loss(output_2_cola_, output_2_cola - output_2_grad.detach(), reduction='sum')
-    cola_loss_2.backward()
-    weight_2_cola_grad = weight_2_cola.grad
-    print(weight_2_cola_grad)
-    print(torch.allclose(weight_2_cola_grad, weight_2_aux_grad, atol=1e-05))
+    if_match_h_1 = torch.allclose(output_1_grad, output_1_aux_grad, atol=1e-03)
+    if_match_h_2 = torch.allclose(output_2_grad, output_2_aux_grad, atol=1e-03)
+    print('[Verify Eq. 5] Gradient of original hidden and auxiliary representation (first layer) : {}, '
+          'Gradient of original hidden and auxiliary representation (second layer): {}'.format(if_match_h_1,
+                                                                                               if_match_h_2))  # Eq. 5
 
-
-    print('--------------Detach--------------------')
+    print('--------------Collaborative Adaptation (first version with detach)--------------------')
     grad_pool = []
-    weight_1_aux.grad = None
-    weight_2_aux.grad = None
-    weight_1.requires_grad = False
-    weight_2.requires_grad = False
-    weight_1_aux.requires_grad = False
-    weight_2_aux.requires_grad = False
-    output_1 = F.linear(input_1, weight_1.t())
-    # with torch.no_grad():
-    #     output_1_aux = F.linear(input_1, weight_1_aux.t()).detach()
-    output_1_aux = F.linear(input_1, weight_1_aux.t())
-    output_1 = output_1 + output_1_aux
-    output_1.requires_grad_(True)
-    output_1.register_hook(backward_hook)
-
-    input_2 = F.relu(output_1)
-    # input_2 = output_1
-    output_2 = F.linear(input_2, weight_2.t())
-    # with torch.no_grad():
-    #     output_2_aux = F.linear(input_2, weight_2_aux.t()).detach()
-    output_2_aux = F.linear(input_2, weight_2_aux.t())
-    output_2 = output_2 + output_2_aux
-    output_2.requires_grad_(True)
-    output_2.register_hook(backward_hook)
-
-    target = torch.zeros((N,), dtype=torch.long)
-    loss = F.cross_entropy(output_2, target, reduction='mean')
+    # Proposed method
+    # Backward original loss
+    # Use the same data and parameters
+    input_1_cola = input_1.clone().detach()
+    weight_1_cola = weight_1.clone().detach()
+    weight_2_cola = weight_2.clone().detach()
+    weight_1_aux_cola = weight_1_aux.clone().detach()
+    weight_2_aux_cola = weight_2_aux.clone().detach()
+    target_cola = target.clone().detach()
+    # Freeze original and auxiliary parameters
+    weight_1_cola.requires_grad = False
+    weight_2_cola.requires_grad = False
+    weight_1_aux_cola.requires_grad = False
+    weight_2_aux_cola.requires_grad = False
+    # First layer
+    output_1_cola = F.linear(input_1_cola, weight_1_cola.t())
+    with torch.no_grad():
+        output_1_aux_cola = F.linear(input_1, weight_1_aux_cola.t()).detach()
+    output_1_cola = output_1_cola + output_1_aux_cola
+    # register hook to retrieve gradient of hidden representations
+    output_1_cola.requires_grad_(True)
+    output_1_cola.register_hook(backward_hook)
+    input_2_cola = F.relu(output_1_cola)
+    # Second layer
+    output_2_cola = F.linear(input_2_cola, weight_2_cola.t())
+    with torch.no_grad():
+        output_2_aux_cola = F.linear(input_2_cola, weight_2_aux_cola.t()).detach()
+    output_2_cola = output_2_cola + output_2_aux_cola
+    # register hook to retrieve gradient of hidden representations
+    output_2_cola.requires_grad_(True)
+    output_2_cola.register_hook(backward_hook)
+    # Compute loss
+    loss = F.cross_entropy(output_2_cola, target_cola, reduction='mean')
     loss.backward()
+    print('Loss: {}'.format(loss))
+
+    # Backward on original parameters
+    # Use the same data and parameters
+    input_1_cola = input_1.clone().detach()
+    weight_1_aux_cola = weight_1_aux_cola.clone().detach()
+    weight_2_aux_cola = weight_2_aux_cola.clone().detach()
+    # Retrieve hidden representations and gradient of hidden representations
+    output_1_aux_cola = output_1_aux_cola.clone().detach()
+    input_2_cola = input_2_cola.clone().detach()
+    output_2_aux_cola = output_2_aux_cola.clone().detach()
     output_1_grad = grad_pool[1]
     output_2_grad = grad_pool[0]
-    input_1_cola = input_1.clone().detach()
-    output_1_cola = output_1_aux.clone().detach()
-    weight_1_cola = weight_1_aux.clone().detach()
-    weight_1_cola.requires_grad = True
-    output_1_cola_ = F.linear(input_1_cola, weight_1_cola.t())
-    cola_loss_1 = 0.5 * F.mse_loss(output_1_cola_, output_1_cola - output_1_grad.detach(), reduction='sum')
+    # First layer
+    weight_1_aux_cola.requires_grad = True
+    output_1_aux_cola_ = F.linear(input_1_cola, weight_1_aux_cola.t())
+    cola_loss_1 = 0.5 * F.mse_loss(output_1_aux_cola_, (output_1_aux_cola - output_1_grad).detach(),
+                                   reduction='sum')  # Proposition 1
     cola_loss_1.backward()
-    weight_1_cola_grad_detach = weight_1_cola.grad
-    print(weight_1_cola_grad_detach)
-    print(torch.allclose(weight_1_cola_grad_detach, weight_1_aux_grad, atol=1e-05))
-    print('-----------------')
-    input_2_cola = input_2.clone().detach()
-    output_2_cola = output_2_aux.clone().detach()
-    weight_2_cola = weight_2_aux.clone().detach()
-    weight_2_cola.requires_grad = True
-    output_2_cola_ = F.linear(input_2_cola, weight_2_cola.t())
-    cola_loss_2 = 0.5 * F.mse_loss(output_2_cola_, output_2_cola - output_2_grad.detach(), reduction='sum')
+    # Second layer
+    weight_2_aux_cola.requires_grad = True
+    output_2_aux_cola_ = F.linear(input_2_cola, weight_2_aux_cola.t())
+    cola_loss_2 = 0.5 * F.mse_loss(output_2_aux_cola_, (output_2_aux_cola - output_2_grad).detach(),
+                                   reduction='sum')  # Proposition 1
     cola_loss_2.backward()
-    weight_2_cola_grad_detach = weight_2_cola.grad
-    print(weight_2_cola_grad_detach)
-    print(torch.allclose(weight_2_cola_grad_detach, weight_2_aux_grad, atol=1e-05))
+    # Verification
+    weight_1_aux_cola_grad = weight_1_aux_cola.grad
+    weight_2_aux_cola_grad = weight_2_aux_cola.grad
+    if_match_g_1 = torch.allclose(weight_1_aux_cola_grad, weight_1_aux_grad, atol=1e-03)
+    if_match_g_2 = torch.allclose(weight_2_aux_cola_grad, weight_2_aux_grad, atol=1e-03)
+    print('[Verify Proposition 1] Gradient of auxiliary parameters (first layer): {}, '
+          'Gradient of auxiliary parameters (second layer): {}'.format(if_match_g_1, if_match_g_2))
+
+    print('--------Collaborative Adaptation---------')
+    grad_pool = []
+    # Proposed method
+    # Backward original loss
+    # Use the same data and parameters
+    input_1_cola = input_1.clone().detach()
+    weight_1_cola = weight_1.clone().detach()
+    weight_2_cola = weight_2.clone().detach()
+    weight_1_aux_cola = weight_1_aux.clone().detach()
+    weight_2_aux_cola = weight_2_aux.clone().detach()
+    target_cola = target.clone().detach()
+    # Freeze original and auxiliary parameters
+    weight_1_cola.requires_grad = False
+    weight_2_cola.requires_grad = False
+    weight_1_aux_cola.requires_grad = False
+    weight_2_aux_cola.requires_grad = False
+    # First layer
+    output_1_cola = F.linear(input_1_cola, weight_1_cola.t())
+    output_1_aux_cola = F.linear(input_1_cola, weight_1_aux_cola.t())
+    output_1_cola = output_1_cola + output_1_aux_cola
+    # register hook to retrieve gradient of hidden representations
+    output_1_cola.requires_grad_(True)
+    output_1_cola.register_hook(backward_hook)
+    input_2_cola = F.relu(output_1_cola)
+    # Second layer
+    output_2_cola = F.linear(input_2_cola, weight_2_cola.t())
+    output_2_aux_cola = F.linear(input_2_cola, weight_2_aux_cola.t())
+    output_2_cola = output_2_cola + output_2_aux_cola
+    # register hook to retrieve gradient of hidden representations
+    output_2_cola.requires_grad_(True)
+    output_2_cola.register_hook(backward_hook)
+    # Compute loss
+    loss = F.cross_entropy(output_2_cola, target_cola, reduction='mean')
+    loss.backward()
+    print('Loss: {}'.format(loss))
+
+    # Backward on original parameters
+    # Use the same data and parameters
+    input_1_cola = input_1.clone().detach()
+    weight_1_aux_cola = weight_1_aux_cola.clone().detach()
+    weight_2_aux_cola = weight_2_aux_cola.clone().detach()
+    # Retrieve hidden representations and gradient of hidden representations
+    output_1_aux_cola = output_1_aux_cola.clone().detach()
+    input_2_cola = input_2_cola.clone().detach()
+    output_2_aux_cola = output_2_aux_cola.clone().detach()
+    output_1_grad = grad_pool[1]
+    output_2_grad = grad_pool[0]
+    # First layer
+    weight_1_aux_cola.requires_grad = True
+    output_1_aux_cola_ = F.linear(input_1_cola, weight_1_aux_cola.t())
+    cola_loss_1 = 0.5 * F.mse_loss(output_1_aux_cola_, (output_1_aux_cola - output_1_grad).detach(),
+                                   reduction='sum')  # Proposition 1
+    cola_loss_1.backward()
+    # Second layer
+    weight_2_aux_cola.requires_grad = True
+    output_2_aux_cola_ = F.linear(input_2_cola, weight_2_aux_cola.t())
+    cola_loss_2 = 0.5 * F.mse_loss(output_2_aux_cola_, (output_2_aux_cola - output_2_grad).detach(),
+                                   reduction='sum')  # Proposition 1
+    cola_loss_2.backward()
+    # Verification
+    weight_1_aux_cola_grad = weight_1_aux_cola.grad
+    weight_2_aux_cola_grad = weight_2_aux_cola.grad
+    if_match_g_1 = torch.allclose(weight_1_aux_cola_grad, weight_1_aux_grad, atol=1e-03)
+    if_match_g_2 = torch.allclose(weight_2_aux_cola_grad, weight_2_aux_grad, atol=1e-03)
+    print('[Verify Proposition 1] Gradient of auxiliary parameters (first layer): {}, '
+          'Gradient of auxiliary parameters (second layer): {}'.format(if_match_g_1, if_match_g_2))
     return
 
 