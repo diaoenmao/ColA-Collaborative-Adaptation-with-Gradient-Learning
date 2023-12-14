44178489403e7c38d0a2547bfab0d94f2c66ea08
diaoenmao <diao_em@hotmail.com>
2023-12-13 21:51:00 -0500

    clean all exp

--- src/process.py
+++ src/process.py
@@ -151,33 +151,11 @@ def process_result(controls):
     for control in controls:
         model_tag = '_'.join(control)
         gather_result(list(control), model_tag, result)
-    exit()
     summarize_result(None, result)
     save(result, os.path.join(result_path, 'processed_result'))
     processed_result = tree()
     extract_result(processed_result, result, [])
     return processed_result
-
-
-# def gather_result(control, model_tag, processed_result):
-#     if len(control) == 1:
-#         exp_idx = exp.index(control[0])
-#         base_result_path_i = os.path.join(result_path, '{}'.format(model_tag))
-#         if os.path.exists(base_result_path_i):
-#             base_result = load(base_result_path_i)
-#             for split in base_result['logger_state_dict']:
-#                 for metric_name in base_result['logger_state_dict'][split]['mean']:
-#                     processed_result[split][metric_name]['mean'][exp_idx] \
-#                         = base_result['logger_state_dict'][split]['mean'][metric_name]
-#                 for metric_name in base_result['logger_state_dict'][split]['history']:
-#                     processed_result[split][metric_name]['history'][exp_idx] \
-#                         = base_result['logger_state_dict'][split]['history'][metric_name]
-#         else:
-#             print('Missing {}'.format(base_result_path_i))
-#             pass
-#     else:
-#         gather_result([control[0]] + control[2:], model_tag, processed_result[control[1]])
-#     return
 
 
 def gather_result(control, model_tag, processed_result):
@@ -191,21 +169,8 @@ def gather_result(control, model_tag, processed_result):
                     processed_result[split][metric_name]['mean'][exp_idx] \
                         = base_result['logger_state_dict'][split]['mean'][metric_name]
                 for metric_name in base_result['logger_state_dict'][split]['history']:
-                    if 'info' in metric_name:
-                        continue
-                    x = base_result['logger_state_dict'][split]['history'][metric_name]
-                    # if len(x) < 40 and len(x) > 10 and 'info' not in metric_name:
-                    if len(x) < 40:
-                        print('a', model_tag, len(x))
-                        num_miss = 40 - len(x)
-                        last_x = x[-1]
-                        x = x + [last_x + 1e-5 * np.random.randn() for _ in range(num_miss)]
-                    if len(x) < 10:
-                        print('b', model_tag, len(x))
-                        continue
-                    # processed_result[split][metric_name]['history'][exp_idx] \
-                    #     = base_result['logger_state_dict'][split]['history'][metric_name]
-                    processed_result[split][metric_name]['history'][exp_idx] = x
+                    processed_result[split][metric_name]['history'][exp_idx] \
+                        = base_result['logger_state_dict'][split]['history'][metric_name]
         else:
             print('Missing {}'.format(base_result_path_i))
             pass
@@ -392,8 +357,7 @@ def make_vis_step(df_history):
                 ax_dict_1[fig_name] = fig[fig_name].add_subplot(111)
             ax_1 = ax_dict_1[fig_name]
             y = df_history[df_name].iloc[0].to_numpy()
-            # y_err = df_history[df_name_std].iloc[0].to_numpy()
-            y_err = 0
+            y_err = df_history[df_name_std].iloc[0].to_numpy()
             x = np.arange(len(y))
             xlabel = 'Epoch'
             pivot = mode
